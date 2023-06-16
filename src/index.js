c520555b15ef8be251dcf3001265079b463ed102
aRakkolainen <103110604+aRakkolainen@users.noreply.github.com>
2023-06-16 16:26:37 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,120 @@
+import "./styles.css";
+//How to insert new row to the table and filling it with content (the function insertNewRow) is implemented based on this tutorial: https://www.tutorialspoint.com/How-to-add-rows-to-a-table-using-JavaScript-DOM
+//How to check the length of table: https://stackoverflow.com/questions/3053503/javascript-to-get-rows-count-of-a-html-table
+
+function insertNewRow(givenUsername) {
+  let table = document.getElementById("content");
+  let givenEmail = document.getElementById("input-email").value;
+  let givenAddress = document.getElementById("input-address").value;
+  let newRow = document.createElement("tr");
+  newRow.setAttribute(
+    "id",
+    "row" + document.getElementById("information-table").rows.length
+  );
+  let rowCount = document.getElementById("information-table").rows.length;
+  let usernameCell = document.createElement("td");
+  usernameCell.setAttribute("id", "username" + rowCount);
+  let emailCell = document.createElement("td");
+  let addressCell = document.createElement("td");
+  let adminstatusCell = document.createElement("td");
+  let pictureCell = document.createElement("td");
+  if (givenUsername !== "") {
+    usernameCell.innerText = givenUsername;
+  }
+  if (givenEmail !== "") {
+    emailCell.innerText = givenEmail;
+  }
+
+  if (givenAddress !== "") {
+    addressCell.innerText = givenAddress;
+  }
+  // Checking the status of checkbox is implemented based on this tutorial: https://www.javascripttutorial.net/javascript-dom/javascript-checkbox/
+  const adminStatusCB = document.getElementById("input-admin");
+
+  if (adminStatusCB.checked === true) {
+    adminstatusCell.innerText = "X";
+  } else {
+    adminstatusCell.innerText = "-";
+  }
+  // This is based on the course material and this tutorial: https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications#example_using_object_urls_to_display_images
+  const imageFile = document.getElementById("input-image").files[0];
+  let imgSrc = "";
+  if (!imageFile) return;
+  imgSrc = URL.createObjectURL(imageFile);
+  let profilePic = document.createElement("div");
+  let pic = document.createElement("img");
+  pic.style.display = "inline-block";
+  pic.src = imgSrc;
+  pic.id = "img" + rowCount;
+  profilePic.appendChild(pic);
+  pictureCell.appendChild(profilePic);
+  newRow.appendChild(usernameCell);
+  newRow.appendChild(emailCell);
+  newRow.appendChild(addressCell);
+  newRow.appendChild(adminstatusCell);
+  newRow.appendChild(pictureCell);
+  table.appendChild(newRow);
+}
+//Hint for this function was taken from this stackoverflow post: https://stackoverflow.com/questions/60874001/how-to-check-if-a-value-exist-in-a-table-and-return-the-value-of-a-cell-in-that
+function checkUsername(givenUsername) {
+  const table = document.getElementById("information-table");
+  let rows = table.rows;
+  let usernameExistence = false;
+  let rowNumber = 1;
+  for (let i = 1; i < rows.length; i++) {
+    if (rows[i].cells[0].innerText === givenUsername) {
+      usernameExistence = true;
+      rowNumber = i;
+      break;
+    }
+  }
+
+  if (usernameExistence === true) {
+    let rowID = "row" + rowNumber;
+    let currentRow = document.getElementById(rowID);
+    let givenEmail = document.getElementById("input-email").value;
+    let givenAddress = document.getElementById("input-address").value;
+    const adminStatusCB = document.getElementById("input-admin");
+    //editing username (not necessary though)
+    currentRow.cells[0].innerText = givenUsername;
+    // Editing email address
+    currentRow.cells[1].innerText = givenEmail;
+    // Editing given address
+    currentRow.cells[2].innerText = givenAddress;
+    // Editing admin status
+    if (adminStatusCB.checked === true) {
+      currentRow.cells[3].innerText = "X";
+    } else {
+      currentRow.cells[3].innerText = "-";
+    }
+
+    // Editing profile image
+    let imgID = "img" + rowNumber;
+    let profilePic = document.getElementById(imgID);
+    let currentImgSrc = profilePic.src;
+    const imageFile = document.getElementById("input-image").files[0];
+    let newImgSrc = "";
+    if (!imageFile) return;
+    newImgSrc = URL.createObjectURL(imageFile);
+
+    if (currentImgSrc !== newImgSrc) {
+      profilePic.src = newImgSrc;
+    }
+  } else {
+    insertNewRow(givenUsername);
+  }
+}
+const submitBtn = document.getElementById("submit-data");
+submitBtn.addEventListener("click", function () {
+  let givenUsername = document.getElementById("input-username").value;
+  checkUsername(givenUsername);
+});
+
+const emptyTableBtn = document.getElementById("empty-table");
+emptyTableBtn.addEventListener("click", function () {
+  let table = document.getElementById("information-table");
+  for (var i = 1; table.rows.length - 1; i = i++) {
+    //How to get the length (number of rows in table): https://www.w3schools.com/jsref/coll_table_rows.asp
+    table.deleteRow(i);
+  }
+});
